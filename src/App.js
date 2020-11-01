de404a832f9f6dcc78d5aad37c5f48b5b888e267
Byungchan-Park <pbc9236@gmail.com>
2020-11-01 17:52:29 +0900

    feat: Set the basic structure of the project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ const App = () => {
   return (
     <div>
       <h1>Let's build a COVID 19 tracker</h1>
+
+      {/* Header */}
+      {/* Title + Select input dropdown filed */}
+
+      {/* InfoBox */}
+      {/* InfoBox */}
+      {/* InfoBox */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   )
 }
